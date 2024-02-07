78e7611ee919c013fd47f1548275f96bb0c5df47
Ana Clara Spadari <anaclaracdesspadari@gmail.com>
2024-02-07 14:40:02 -0300

    Implementating a switch menu with two other menus inside (one for books and the other for clients)

--- src/main/java/ecommerceBooksMaven/application/Application.java
+++ src/main/java/ecommerceBooksMaven/application/Application.java
@@ -10,40 +10,65 @@ public class Application {
 
 	public static void main(String[] args) {
 		ProductConnection p = new ProductConnection();
-	/*	EntityManagerFactory emf = Persistence.createEntityManagerFactory("books-jpa");
+		
+	 	/* EntityManagerFactory emf = Persistence.createEntityManagerFactory("books-jpa");
 		EntityManager em = emf.createEntityManager();
 		
-		Client cl = new Client(null,"mulher maravilha", "gui.amapa22@gmail.com");
-		em.getTransaction().begin();
-		em.persist(cl);
-		em.getTransaction().commit();
-		System.out.println("Tabela criada!");
 		em.close();
 		emf.close();
 		
 		*/
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Bem vindo ao E-commerce Books! O que você gostaria de visualizar?\n\n(1)Listar produtos\n(2)Buscar produto\n(3)Cadastrar pedido\n(4)Atualizar produto\n(5)Excluir produto\n(0)Sair");
-		int option=sc.nextInt();
-		switch(option){
+		System.out.println("Bem vindo ao E-commerce Books! O que você gostaria de visualizar?\n\n(1)Produtos\n(2)Clientes(0)Sair");
+		int option1=sc.nextInt();
+		
+		switch(option1) {
 			case 1:
-			System.out.println("Lista de produtos");
-			break;
+				System.out.println("Menu de Clientes\n\n(1)Listar todos os clientes\n(2)Pesquisar cliente\n(3)Cadastrar cliente\n(4)Atualizar dados do cliente\n(5)Excluir cliente\n(0)Voltar");
+				int optionClient=sc.nextInt();
+				switch(optionClient){
+					case 1:
+					System.out.println("Lista de clientes");
+					break;
+					case 2:
+					System.out.println("Pesquisar cliente");
+					break;
+					case 3:
+					System.out.println("Cadastrar novo cliente");
+					break;
+					case 4:
+					System.out.println("Atualizar dados do cliente");
+					case 5:
+					System.out.println("Excluir dados do cliente");
+					break;
+					default:
+					System.out.println("Ok, até mais!");
+					break;
+			}
 			case 2:
-			System.out.println("Pesquisar produto");
-			break;
-			case 3:
-			System.out.println("Cadastrar pedido");
-			break;
-			case 4:
-			System.out.println("Atualizar produto");
-			case 5:
-			System.out.println("Excluir produto");
-			break;
-			default:
-			System.out.println("Ok, até mais!");
-			break;
+				int optionBooks=sc.nextInt();
+				switch(optionBooks){
+					case 1:
+					System.out.println("Lista de produtos");
+					break;
+					case 2:
+					System.out.println("Pesquisar produto");
+					break;
+					case 3:
+					System.out.println("Cadastrar pedido");
+					break;
+					case 4:
+					System.out.println("Atualizar produto");
+					case 5:
+					System.out.println("Excluir produto");
+					break;
+					default:
+					System.out.println("Ok, até mais!");
+					break;
+				}
 		}
+		
+		
 
 		sc.close();
 		
