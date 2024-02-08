38aa9dc59998d70832b05bf2009eb0122203a0d2
Christophermarcelinoribeiro <gui.amapa66@gmail.com>
2024-02-07 23:21:11 -0300

    Replaced null fields with quotation marks

--- src/main/java/ecommerceBooksMaven/exceptions/ClientException.java
+++ src/main/java/ecommerceBooksMaven/exceptions/ClientException.java
@@ -6,62 +6,96 @@ import ecommerceBooksMaven.entities.Client;
 
 public class ClientException {
 	
-	
 	public void nameEmpty(Client c) {
 		if(c.getName() == null) {
 			throw new IllegalStateException("{\r\n"
 					+ " \"code\": 400,\r\n"
 					+ " \"status\": \"Bad Request\",\r\n"
-					+ " \"message\": \"O campo 'nome' ï¿½ obrigatï¿½rio.\",\r\n"
+					+ " \"message\": \"O campo 'nome' é obrigatório.\",\r\n"
 					+ " \"details\": [\r\n"
 					+ " {\r\n"
 					+ " \"field\": \"nome\",\r\n"
-					+ " \"message\": \"O campo 'nome' ï¿½ obrigatï¿½rio.\"\r\n"
-					+ " }\r\n"
+					+ " \"message\": \"O campo 'nome' é obrigatório.\"\r\n"
+					+ " 	}\r\n"
 					+ " ]\r\n"
-					+ "}");
+					+ "}"); 
 		} 
-		
+		 
 	}
 	
+	public void emptyFields(Client c) {
+		if(c.getName() == null) {
+			throw new IllegalStateException("{\r\n"
+					+ " \"code\": 400,\r\n"
+					+ " \"status\": \"Bad Request\",\r\n"
+					+ " \"message\": \"O campo 'nome' é obrigatório.\",\r\n"
+					+ " \"details\": [\r\n"
+					+ " {\r\n"
+					+ " \"field\": \"nome\",\r\n"
+					+ " \"message\": \"O campo 'nome' é obrigatório.\"\r\n"
+					+ " 	}\r\n"
+					+ " ]\r\n"
+					+ "}"); 
+		} 
+		if(c.getAddress() == null) {
+			throw new IllegalStateException("{\r\n"
+			+ " \"code\": 400,\r\n"
+					+ " \"status\": \"Bad Request\",\r\n"
+					+ " \"message\": \"O campo 'adress' é obrigatório.\",\r\n"
+					+ " \"details\": [\r\n"
+					+ " {\r\n"
+					+ " \"field\": \"adress\",\r\n"
+					+ " \"message\": \"O campo 'adress' é obrigatório.\"\r\n"
+					+ " 	}\r\n"
+					+ " ]\r\n"
+					+ "}"); 
+		}
+		if(c.getCpf() == null) {
+			throw new IllegalStateException("{\r\n"
+			+ " \"code\": 400,\r\n"
+					+ " \"status\": \"Bad Request\",\r\n"
+					+ " \"message\": \"O campo 'cpf' é obrigatório.\",\r\n"
+					+ " \"details\": [\r\n"
+					+ " {\r\n"
+					+ " \"field\": \"cpf\",\r\n"
+					+ " \"message\": \"O campo 'cpf' é obrigatório.\"\r\n"
+					+ " 	}\r\n"
+					+ " ]\r\n"
+					+ "}");  
+		}
+		if(c.getTelephone() == null) {
+			throw new IllegalStateException("{\r\n"
+			+ " \"code\": 400,\r\n"
+					+ " \"status\": \"Bad Request\",\r\n"
+					+ " \"message\": \"O campo 'telephone' é obrigatório.\",\r\n"
+					+ " \"details\": [\r\n"
+					+ " {\r\n"
+					+ " \"field\": \"telephone\",\r\n"
+					+ " \"message\": \"O campo 'telephone' é obrigatório.\"\r\n"
+					+ " 	}\r\n"
+					+ " ]\r\n"
+					+ "}"); 
+		}
+	
+		
+	}
 	public void checkEmail(List<Client> c, Client cc) { 
 		for(Client t: c) {
 			if(t.equals(cc)) {
-				throw new IllegalStateException("{\r\n"
+				throw new IllegalStateException("{\r\n" 
 						+ " \"code\": 400,\r\n"
 						+ " \"status\": \"Bad Request\",\r\n"
-						+ " \"message\": \"Nï¿½o foi possivel criar cadastro do cliente.\",\r\n"
+						+ " \"message\": \"Não foi possivel criar cadastro do cliente.\",\r\n"
 						+ " \"details\": [\r\n"
 						+ " {\r\n"
 						+ " \"field\": \"email\",\r\n"
-						+ " \"message\": \"e-mail de endereï¿½o jï¿½ existe.\"\r\n"
-						+ " }\r\n"
+						+ " \"message\": \"e-mail de endereço já existe.\"\r\n"
+						+ " 	}\r\n"
 						+ " ]\r\n"
-						+ "}");
+						+ "}"); 
 			} 
 		}
 	} 
-//	 public void checkTable(Integer i) {
-//		 ProductConnection p = new ProductConnection();
-//		 Client obj = new Client();
-//		 obj =  p.findById(i);
-//		 if(obj == null) {
-//			 throw new IllegalStateException("{\r\n"
-//						+ " \"code\": 400,\r\n"
-//						+ " \"status\": \"Bad Request\",\r\n"
-//						+ " \"message\": \"Nï¿½o foi possivel recuperar as informaï¿½ï¿½es do clinte.\",\r\n"
-//						+ " \"details\": [\r\n"
-//						+ " 		{\r\n"
-//						+ "		   \"field\": \"*\",\r\n"
-//						+ " 		\"message\": \"tabela cliente vazia ou nï¿½o hï¿½ o Id exigido.\"\r\n"
-//						+ " 		}\r\n"
-//						+ " 	]\r\n"
-//						+ "}");
-//		 }else {
-//			 System.out.println(obj);
-//		 }
-//			
-//		
-//	 }
+	
 	
 }
